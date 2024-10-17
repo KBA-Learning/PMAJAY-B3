066d1d13589b7a7fb6401c25dca8c3948c93932e
pglekshmi <pglekshmi88@gmail.com>
2024-10-17 14:59:41 +0530

    updated

---
+++ 29-Backend/KBA-Backend/index.js
@@ -0,0 +1,44 @@
+import express,{json} from 'express';
+import bcrypt from 'bcrypt';
+
+
+
+const app=express();
+app.use(json())
+
+const port=8000;
+const user=new Map();
+
+app.get('/',(req,res)=>{
+   res.send("Hello World");
+})
+
+app.post('/signup',async(req,res)=>{
+    console.log("Hi");
+    const data = req.body;
+    console.log(data.FirstName);
+    const {FirstName,
+    LastName,
+    UserName,
+    Password,
+    Role}= data;
+    console.log(FirstName);
+    const newP= await bcrypt.hash(Password,10)
+    console.log(newP);
+    user.set(UserName,{
+       FirstName,LastName,Password:newP,Role 
+    });
+    
+    console.log(user.get(UserName));
+    // res.status(201).send("Data Saved");
+    res.status(201).json({message:"Data Saved"});
+    
+
+
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is listening to ${port}`)
+})
