4a80fbd68d4161fe0ad596da1f43a0d1fcd16656
pglekshmi <pglekshmi88@gmail.com>
2024-10-23 12:11:32 +0530

    updated

--- 33-Backend/KBA-Backend/index.js
+++ 33-Backend/KBA-Backend/index.js
@@ -1,9 +1,13 @@
 import express,{json} from 'express';
 import dotenv from 'dotenv';
 import { adminRoute } from './Routes/adminRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 const app=express();
+app.use(cors({
+    origin:'*'
+}))
 app.use(json());
 app.use('/',adminRoute)
 
